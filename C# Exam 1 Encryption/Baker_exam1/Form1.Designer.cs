3e62ae65c7488fbbfb632ea2ee2323dd821ea1e9
Chris B <mr.baker025@gmail.com>
2022-01-17 07:41:18 -0500

    Add File to Repo

---
+++ C# Exam 1 Encryption/Baker_exam1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Baker_exam1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.btnCalcLetters = new System.Windows.Forms.Button();
+            this.txtBoxUserLetter = new System.Windows.Forms.TextBox();
+            this.btnCalcEnc = new System.Windows.Forms.Button();
+            this.lstBoxResults = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(2, 9);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(478, 23);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Enter the Letter You want to search:";
+            // 
+            // btnCalcLetters
+            // 
+            this.btnCalcLetters.Location = new System.Drawing.Point(273, 55);
+            this.btnCalcLetters.Name = "btnCalcLetters";
+            this.btnCalcLetters.Size = new System.Drawing.Size(139, 32);
+            this.btnCalcLetters.TabIndex = 1;
+            this.btnCalcLetters.Text = "Generate";
+            this.btnCalcLetters.UseVisualStyleBackColor = true;
+            this.btnCalcLetters.Click += new System.EventHandler(this.btnCalcLetters_Click);
+            // 
+            // txtBoxUserLetter
+            // 
+            this.txtBoxUserLetter.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtBoxUserLetter.ForeColor = System.Drawing.Color.DarkOrange;
+            this.txtBoxUserLetter.Location = new System.Drawing.Point(68, 58);
+            this.txtBoxUserLetter.Name = "txtBoxUserLetter";
+            this.txtBoxUserLetter.Size = new System.Drawing.Size(100, 29);
+            this.txtBoxUserLetter.TabIndex = 2;
+            // 
+            // btnCalcEnc
+            // 
+            this.btnCalcEnc.Location = new System.Drawing.Point(38, 366);
+            this.btnCalcEnc.Name = "btnCalcEnc";
+            this.btnCalcEnc.Size = new System.Drawing.Size(118, 36);
+            this.btnCalcEnc.TabIndex = 3;
+            this.btnCalcEnc.Text = "Encrypt";
+            this.btnCalcEnc.UseVisualStyleBackColor = true;
+            this.btnCalcEnc.Click += new System.EventHandler(this.btnCalcEnc_Click);
+            // 
+            // lstBoxResults
+            // 
+            this.lstBoxResults.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lstBoxResults.Font = new System.Drawing.Font("OCR A Extended", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBoxResults.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lstBoxResults.FormattingEnabled = true;
+            this.lstBoxResults.ItemHeight = 12;
+            this.lstBoxResults.Location = new System.Drawing.Point(54, 135);
+            this.lstBoxResults.Name = "lstBoxResults";
+            this.lstBoxResults.Size = new System.Drawing.Size(358, 184);
+            this.lstBoxResults.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(172, 366);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(117, 36);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(304, 366);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(119, 36);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(480, 438);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstBoxResults);
+            this.Controls.Add(this.btnCalcEnc);
+            this.Controls.Add(this.txtBoxUserLetter);
+            this.Controls.Add(this.btnCalcLetters);
+            this.Controls.Add(this.lblPrompt);
+            this.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.Name = "Form1";
+            this.Text = "EXAM ONE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Button btnCalcLetters;
+        private System.Windows.Forms.TextBox txtBoxUserLetter;
+        private System.Windows.Forms.Button btnCalcEnc;
+        private System.Windows.Forms.ListBox lstBoxResults;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ C# Exam 1 Encryption/Baker_exam1/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Baker_exam1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        //Christopher Baker
+        //Exam 1 
+        //October-4-2021
+
+       
+        //search through the letters of the file. 
+        private void btnCalcLetters_Click(object sender, EventArgs e)
+        {
+           
+            //list of hhlyrics file. This is not reading the file correctly 
+            List<string> hhLyricsList = new List<string>();
+
+
+            //Declare Variables. 
+            int totalLetters;
+            int letterCount = 0;
+            string userLetters;
+
+            //try catch for validation and exception errors countLetters(). 
+            try
+            {
+                //Declare Object To access File
+                StreamReader inputFile;
+               
+                //Open the file
+                inputFile = File.OpenText("..\\..\\hhlyrics.txt");
+                
+                
+                //Loop to keep accessing lines in the file and make
+                //sure it does not throw an exception reading past the data
+                while (!inputFile.EndOfStream)
+                {
+                    //Read a line from the file and place into boysFirstNameList List. 
+                    hhLyricsList.Add(inputFile.ReadLine());
+
+                }
+
+                //Close the file
+                inputFile.Close();
+            }
+            //catch statement if file is not found. 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            foreach (string txt in hhLyricsList)
+            {
+                userLetters = txtBoxUserLetter.Text;
+                //create array of user letters so it can compare values in the for loop. 
+                char[] userCharacters = userLetters.ToCharArray();
+
+                //for loop to iterate throught the str1 string. 
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    //if statment to compare all items in the str1 array to the 
+                    //users input. 
+                    if (txt[i] == userCharacters[0])
+                    {
+                        //Counts the amount of times it recognizes the letter.
+                        letterCount++;
+                    }
+
+                }
+            }     
+        
+            //Call the countLetters method and place returned value into totalLetters
+            totalLetters = letterCount;
+
+            //Display results of the totalLetters to listbox. Cannot understand why
+            //this is displaying the error message and not the contents of the file.
+            lstBoxResults.Items.Add("There are " + totalLetters.ToString() + " of those letters total in the file.");
+
+
+        }
+
+       
+
+
+        //encrypt method. 
+        private void btnCalcEnc_Click(object sender, EventArgs e)
+        {
+            //list of hhlyrics file. This is not reading the file correctly 
+            List<string> hhLyricsList = new List<string>();
+
+
+            string newLetter = "";
+
+            //try catch for validation and exception errors countLetters(). 
+            try
+            {
+                //Declare Object To access File
+                StreamReader inputFile;
+
+                //Open the file
+                inputFile = File.OpenText("..\\..\\hhlyrics.txt");
+
+
+                //Loop to keep accessing lines in the file and make
+                //sure it does not throw an exception reading past the data
+                while (!inputFile.EndOfStream)
+                {
+                    //Read a line from the file and place into boysFirstNameList List. 
+                    hhLyricsList.Add(inputFile.ReadLine());
+
+                }
+
+                //Close the file
+                inputFile.Close();
+            }
+            //catch statement if file is not found. 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //foreach statment sets name variable to string and searches 
+            // in hhLyricsList
+            foreach (string txt in hhLyricsList)
+            {
+                char[] CharactersCapitols = { 'A', 'D', 'H', 'K', 'M', 'O', 'S', 'X', '\n'};
+
+                char[] CharactersLowerCase = { 'a', 'd', 'h', 'k', 'm', 'o', 's', 'x'};
+
+                //for loop to iterate throught the txt string. 
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    // if statement say if any of the letters are there change to newLetter value.
+                    if (txt[i] == CharactersCapitols[0])
+                    {
+                        newLetter += 'Z';
+
+                    }
+                    else if (txt[i] == CharactersLowerCase[0])
+                    {
+                        newLetter += 'z';
+                    }
+                    else if (txt[i] == CharactersCapitols[1])
+                    {
+                        newLetter += 'W';
+                    }
+                    else if (txt[i] == CharactersLowerCase[1])
+                    {
+                        newLetter += 'w';
+                    }
+                    else if (txt[i] == CharactersCapitols[2])
+                    {
+                        newLetter += 'T';
+                    }
+                    else if (txt[i] == CharactersLowerCase[2])
+                    {
+                        newLetter += 't';
+                    }
+                    else if (txt[i] == CharactersCapitols[3])
+                    {
+                        newLetter += 'R';
+                    }
+                    else if (txt[i] == CharactersLowerCase[3])
+                    {
+                        newLetter += 'r';
+                    }
+                    else if (txt[i] == CharactersCapitols[4])
+                    {
+                        newLetter += 'P';
+                    }
+                    else if (txt[i] == CharactersLowerCase[4])
+                    {
+                        newLetter += 'p';
+                    }
+                    else if (txt[i] == CharactersCapitols[5])
+                    {
+                        newLetter += 'N';
+                    }
+                    else if (txt[i] == CharactersLowerCase[5])
+                    {
+                        newLetter += 'n';
+                    }
+                    else if (txt[i] == CharactersCapitols[6])
+                    {
+                        newLetter += 'J';
+                    }
+                    else if (txt[i] == CharactersLowerCase[6])
+                    {
+                        newLetter += 'j';
+                    }
+                    else if (txt[i] == CharactersCapitols[7])
+                    {
+                        newLetter += 'E';
+                    }
+                    else if (txt[i] == CharactersLowerCase[7])
+                    {
+                        newLetter += 'e';
+                    }
+                    else if (txt[i] == CharactersCapitols[8])
+                    {
+                        newLetter += '\n';
+                    }
+                    //else statement to put back anything that isn't in the lower or capitols arrays. 
+                    else
+                    {
+                        newLetter += txt[i];
+                    }
+
+                }
+            }
+
+            //Tried to display to the list box but cannot figure out
+            //how to format the string to fit inside the list box. 
+            //The format did not stay the same when I output the file. 
+            lstBoxResults.Items.Add(newLetter.ToString());
+
+            //I just used a message box instead to display the encrypted file contents. 
+            MessageBox.Show(newLetter.ToString());
+
+        }
+        //method to clear text boxes.
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //code to clear textboxes.
+            txtBoxUserLetter.Clear();
+            lstBoxResults.Items.Clear();
+        }
+        //method to close the program.
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //code to close. 
+            this.Close();
+        }
+    }
+}
